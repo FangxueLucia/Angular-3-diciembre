d71e443882249ea4d6b676ec509331c3e29a5d3a
Lu <elgranchipiron@outlook.es>
2025-12-03 11:30:01 +0100

    for

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-
+switch = signal(false);
 }
